e2c643bac11ad370e91e9ad53f338de0e47bdcf2
George Escobar <geo.escobar214@gmail.com>
2022-04-22 15:54:23 -0500

    adding error alert

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,20 @@ const button = document.querySelector(".form button");
 const qrImg = document.querySelector(".qr-img img");
 const title = document.querySelector("title");
 
-title.style.display = "block";
 
-button.addEventListener("click",function(event){
-    event.preventDefault();
-  let data = input.value;
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+  loadCode();
+});
+
+function loadCode() {
+  let data = input.value.trim();
   if(data.length > 0){
     let imgSrc = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${data}`;
     qrImg.src = imgSrc;
     title.style.display = "none";
+  } else {
+    alert('Please enter valid URL');
+    return loadCode;
   }
-})
-
+}
